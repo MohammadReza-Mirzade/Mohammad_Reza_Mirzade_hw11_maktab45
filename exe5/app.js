1023f380d83883d99d874e8dff3d391c4bbc919c
MohammadR-M <mohammadrezamirzadejava@gmail.com>
2021-02-05 14:00:30 +0330

    Complete exe5.

---
+++ exe5/app.js
@@ -0,0 +1,6 @@
+const calculator = require("./modules/calculator");
+
+calculator.subtraction(27, 17);
+calculator.addition(7, 8);
+calculator.multiplication(7, 8);
+calculator.division(12, 3);
---
+++ exe5/modules/calculator.js
@@ -0,0 +1,14 @@
+exports.subtraction = function (a, b){
+    console.log(a - b);
+}
+
+exports.addition = function (a, b){
+    console.log(a + b);
+}
+exports.multiplication = function (a, b){
+    console.log(a * b);
+}
+
+exports.division = function (a, b){
+    console.log(a / b);
+}
